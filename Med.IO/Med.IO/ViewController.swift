831e16491ddaef4390a215e118d9d9fd5ff9510b
jezza23 <JezzaGaming@Jeremys-MacBook.local>
2016-02-13 23:08:08 -0500

    initial commit

---
+++ Med.IO/Med.IO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Med.IO
+//
+//  Created by Jeremy Irvine on 2/13/16.
+//  Copyright © 2016 Jeremy Irvine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
